f908a7f753e7089e63287ed2f32fc14514a17076
Infifu <iliiniliya2008@gmail.com>
2025-03-25 20:23:17 +0200

    added a lot of functions

--- DatabaseAccess.cpp
+++ DatabaseAccess.cpp
@@ -3,7 +3,7 @@
 
 bool DatabaseAccess::open()
 {
-	int res = sqlite3_open("Gallery.VC.db", &db);
+    int res = sqlite3_open("Gallery.VC.db", &db);
 
     if (res != SQLITE_OK)
     {
@@ -43,16 +43,64 @@ void DatabaseAccess::clear()
 
 const std::list<Album> DatabaseAccess::getAlbums()
 {
-    return std::list<Album>();
+    std::list<Album> albums;
+    std::string sqlStatement = "SELECT * FROM ALBUMS;";
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+    }
+
+    while ((res = sqlite3_step(stmt)) == SQLITE_ROW)
+    {
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        std::string creationDate = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        int user_ID = sqlite3_column_int(stmt, 3);
+        albums.push_back(Album(user_ID, name, creationDate));
+    }
+
+    sqlite3_finalize(stmt);
+    return albums;
 }
 
 const std::list<Album> DatabaseAccess::getAlbumsOfUser(const User& user)
 {
-    return std::list<Album>();
+    std::list<Album> albums;
+    std::string sqlStatement = "SELECT * FROM ALBUMS WHERE USER_ID = " + std::to_string(user.getId()) + ";";
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+    }
+
+    while ((res = sqlite3_step(stmt)) == SQLITE_ROW)
+    {
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        std::string creationDate = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        int user_ID = sqlite3_column_int(stmt, 3);
+        albums.push_back(Album(user_ID, name, creationDate));
+    }
+
+    sqlite3_finalize(stmt);
+    return albums;
 }
 
 void DatabaseAccess::createAlbum(const Album& album)
 {
+    std::string sqlStatement = "INSERT INTO ALBUMS (NAME, CREATION_DATE, USER_ID) VALUES ('" + album.getName() + "', '" + album.getCreationDate() + "', " + std::to_string(album.getOwnerId()) + ");";
+    char* errMessage = nullptr;
+    int res = sqlite3_exec(db, sqlStatement.c_str(), nullptr, nullptr, &errMessage);
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: " << errMessage << std::endl;
+        sqlite3_free(errMessage);
+    }
 }
 
 void DatabaseAccess::deleteAlbum(const std::string& albumName, int userId)
@@ -71,11 +119,55 @@ void DatabaseAccess::deleteAlbum(const std::string& albumName, int userId)
 
 bool DatabaseAccess::doesAlbumExists(const std::string& albumName, int userId)
 {
-    return false;
+    std::string sqlStatement = "SELECT * FROM ALBUMS WHERE name = '" + albumName + "';";
+
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return false;
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        sqlite3_finalize(stmt);
+        return true;
+    }
+    else
+    {
+        sqlite3_finalize(stmt);
+        return false;
+    }
 }
 
 Album DatabaseAccess::openAlbum(const std::string& albumName)
 {
+    std::string sqlStatement = "SELECT * FROM ALBUMS WHERE name = '" + albumName + "';";
+
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return Album();
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        int userID = sqlite3_column_int(stmt, 4);
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        std::string creation_date = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+        sqlite3_finalize(stmt);
+        return Album(userID, name, creation_date);
+    }
+    sqlite3_finalize(stmt);
     return Album();
 }
 
@@ -86,15 +178,74 @@ void DatabaseAccess::closeAlbum(Album& pAlbum)
 
 void DatabaseAccess::printAlbums()
 {
+    std::string sqlStatement = "SELECT * FROM ALBUMS;";
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return;
+    }
+
+    while ((res = sqlite3_step(stmt)) == SQLITE_ROW)
+    {
+        int userID = sqlite3_column_int(stmt, 4);
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        std::string creation_date = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+        std::cout << Album(userID, name, creation_date) << std::endl;
+    }
+
+    sqlite3_finalize(stmt);
+    return;
 }
 
 void DatabaseAccess::printUsers()
 {
+    std::string sqlStatement = "SELECT * FROM USERS;";
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return;
+    }
+
+    while ((res = sqlite3_step(stmt)) == SQLITE_ROW)
+    {
+        int userID = sqlite3_column_int(stmt, 0);
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        std::cout << User(userID, name) << std::endl;
+    }
+
+    sqlite3_finalize(stmt);
+    return;
 }
 
 User DatabaseAccess::getUser(int userId)
 {
-    return User(1,"");
+    std::string sqlStatement = ("SELECT * FROM USERS WHERE ID = " + std::to_string(userId));
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return User(-1, "ERROR");
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        std::string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        sqlite3_finalize(stmt);
+        return User(userId, name);
+    }
+    return User(-1, "ERROR");
 }
 
 void DatabaseAccess::createUser(User& user)
@@ -123,12 +274,35 @@ void DatabaseAccess::deleteUser(const User& user)
 
 bool DatabaseAccess::doesUserExists(int userId)
 {
-    return false;
+    std::string sqlStatement = "SELECT * FROM USERS WHERE ID = " + std::to_string(userId) + ";";
+
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return false;
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        sqlite3_finalize(stmt);
+        return true;
+    }
+    else
+    {
+        sqlite3_finalize(stmt);
+        return false;
+    }
 }
 
 int DatabaseAccess::countAlbumsOwnedOfUser(const User& user)
 {
-    return 0;
+    std::list<Album> albums = getAlbumsOfUser(user);
+    return albums.size();
 }
 
 int DatabaseAccess::countAlbumsTaggedOfUser(const User& user)
@@ -153,7 +327,7 @@ User DatabaseAccess::getTopTaggedUser()
 
 Picture DatabaseAccess::getTopTaggedPicture()
 {
-    return Picture(1,"");
+    return Picture(1, "");
 }
 
 std::list<Picture> DatabaseAccess::getTaggedPicturesOfUser(const User& user)
@@ -163,10 +337,68 @@ std::list<Picture> DatabaseAccess::getTaggedPicturesOfUser(const User& user)
 
 void DatabaseAccess::addPictureToAlbumByName(const std::string& albumName, const Picture& picture)
 {
+    std::string sqlStatement = "SELECT * FROM ALBUMS WHERE NAME = '" + albumName + "';";
+
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return;
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        int albumID = sqlite3_column_int(stmt, 0);
+        sqlite3_finalize(stmt);
+
+        sqlStatement = "INSERT INTO PICTURES (NAME,LOCATION,CREATION_DATE,ALBUM_ID) VALUES (";
+        sqlStatement += "'" + picture.getName() + "', '" + picture.getPath() + "', '" + picture.getCreationDate() + "', " + std::to_string(picture.getId()) + ");";
+
+        res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+        if (res != SQLITE_OK)
+        {
+            std::cerr << "error: Failed to prepare statement" << std::endl;
+            return;
+        }
+    }
 }
 
 void DatabaseAccess::removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName)
 {
+    std::string sqlStatement = "SELECT * FROM ALBUMS WHERE NAME = '" + albumName + "';";
+
+    sqlite3_stmt* stmt;
+    char* errMessage = nullptr;
+    int res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+    if (res != SQLITE_OK)
+    {
+        std::cerr << "error: Failed to prepare statement" << std::endl;
+        return;
+    }
+
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_ROW)
+    {
+        int albumID = sqlite3_column_int(stmt, 0);
+        sqlite3_finalize(stmt);
+
+        //find the pictures with matching albumID
+        sqlStatement = "DELETE FROM PICTURES WHERE ID = " + std::to_string(albumID) + " AND NAME = '" + pictureName + "';";
+        res = sqlite3_prepare_v2(db, sqlStatement.c_str(), -1, &stmt, nullptr);
+
+        if (res != SQLITE_OK)
+        {
+            std::cerr << "error: Failed to prepare statement" << std::endl;
+            return;
+        }
+        sqlite3_finalize(stmt);
+    }
 }
 
 void DatabaseAccess::tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId)
@@ -187,7 +419,7 @@ void DatabaseAccess::tagUserInPicture(const std::string& albumName, const std::s
     if (res == SQLITE_ROW)
     {
         int pictureId = sqlite3_column_int(stmt, 0);
-        sqlite3_finalize(stmt); 
+        sqlite3_finalize(stmt);
 
         sqlStatement = "INSERT INTO TAGS (PICTURE_ID, USER_ID) VALUES ('" + std::to_string(pictureId) + "', '" + std::to_string(userId) + "');";
         res = sqlite3_exec(db, sqlStatement.c_str(), nullptr, nullptr, &errMessage);
@@ -200,7 +432,7 @@ void DatabaseAccess::tagUserInPicture(const std::string& albumName, const std::s
     }
     else
     {
-        sqlite3_finalize(stmt); 
+        sqlite3_finalize(stmt);
         return;
     }
 }
--- DatabaseAccess.h
+++ DatabaseAccess.h
@@ -8,30 +8,30 @@
 class DatabaseAccess : public IDataAccess
 {
 public:
-    const std::list<Album> getAlbums() override;
-    const std::list<Album> getAlbumsOfUser(const User& user) override;
-    void createAlbum(const Album& album) override;
+    const std::list<Album> getAlbums() override; //done
+    const std::list<Album> getAlbumsOfUser(const User& user) override; //done
+    void createAlbum(const Album& album) override; //done
     void deleteAlbum(const std::string& albumName, int userId) override; //done
-    bool doesAlbumExists(const std::string& albumName, int userId) override;
-    Album openAlbum(const std::string& albumName) override;
+    bool doesAlbumExists(const std::string& albumName, int userId) override; //done
+    Album openAlbum(const std::string& albumName) override; //done
     void closeAlbum(Album& pAlbum) override; //done
-    void printAlbums() override;
+    void printAlbums() override; //done
 
     // picture related
-    void addPictureToAlbumByName(const std::string& albumName, const Picture& picture) override;
-    void removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName) override;
-    void tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override; //in work rn
-    void untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override; //in work rn
+    void addPictureToAlbumByName(const std::string& albumName, const Picture& picture) override; //done needs review
+    void removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName) override; //done needs review
+    void tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override; //done
+    void untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override; //done
 
     // user related
-    void printUsers() override;
-    User getUser(int userId) override;
+    void printUsers() override; //done
+    User getUser(int userId) override; //done needs review
     void createUser(User& user) override; //done
     void deleteUser(const User& user) override; //done
-    bool doesUserExists(int userId) override;
+    bool doesUserExists(int userId) override; //done
 
     // user statistics
-    int countAlbumsOwnedOfUser(const User& user) override;
+    int countAlbumsOwnedOfUser(const User& user) override; //done
     int countAlbumsTaggedOfUser(const User& user) override;
     int countTagsOfUser(const User& user) override;
     float averageTagsPerAlbumOfUser(const User& user) override;
@@ -43,10 +43,10 @@ public:
 
     bool open() override; //done
     void close() override; //done
-    void clear() override; //done
+    void clear() override; //to do
 
 private:
-	sqlite3* db;
+    sqlite3* db;
 
 
 };
